f4eb3b87aa532e08956ab05a7fd0ce61268bbf36
Thomas <tjohannes.ee@gmail.com>
2017-03-08 00:16:08 -0800

    Update FirstRreverse.js

--- Easy Difficulty/FirstRreverse.js
+++ Easy Difficulty/FirstRreverse.js
@@ -1,10 +1,8 @@
-function FirstRreverse(str){
-  var output = "";
-  
-  for (var i = str.length -1; i >= 0; i--){
-    output = output + str.charAt(i)
-  }
-  return output;
+function FirstRreverse(str) {
+    let output = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        output = output + str.charAt(i)
+    }
+    return output;
 }
-
 console.log(FirstRreverse("coderbyte"))
