b504691473be1b6825af131c23590d7da0b38a86
Thomas <tjohannes.ee@gmail.com>
2017-03-08 00:36:43 -0800

    Update LetterChange.js

--- Easy Difficulty/LetterChange.js
+++ Easy Difficulty/LetterChange.js
@@ -1,3 +1,7 @@
+//This challenge requires you to change every letter in the string to the letter following
+//it in the alphabet, so a becomes b, z becomes a, etc. Once every letter is changed,
+//we then need to capitalize only the vowels, namely: a, e, i, o, u.
+
 function LetterChange(str) {
 
     var letters = str.replace(/[a-z0-9]/gim, function(char) {
